8efd7516c0537f7c388654071e361e447bbec254
CrisRRo <rcristina76@yahoo.com>
2018-08-06 13:37:38 +0300

    Add Mapbox key and viewport for responsiveness

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiY3Jpc3IiLCJhIjoiY2praTNnOHhiMGJwdzNxcGh6YmU3ejdscSJ9.cpuK7DyOy58rU1-ZXyQ16w',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
